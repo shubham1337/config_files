02402392e7ac38af045197e5298d712f3d278e35
Shubham Sharma <shubham1337@users.noreply.github.com>
2019-04-24 20:24:03 +0530

    Commented out Oh My Zsh

--- install_deps.sh
+++ install_deps.sh
@@ -5,7 +5,7 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # Oh My Zsh Config
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+#sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # Getting custom config files
 git clone https://github.com/shubham1337/config_files .
